8880265860db259ab05734a7c5f2865205d7cd0a
tanniakaye <tane4kacol@bk.ru>
2021-01-14 19:55:39 +0000

    simple program for iteration of two lists at the same time

---
+++ simpleprogram.py
@@ -0,0 +1,12 @@
+# simple python program to test GitHub
+# combines items from both lists and creates a new list
+# iterating over two lists simultaniously
+
+first_list = [1, 2, 3, 4]
+second_list = ["one", "two", "three", "four"]
+combined_list = []
+
+for number, word in zip(first_list, second_list):
+    combined_list.append((number, word))
+
+print(combined_list)
